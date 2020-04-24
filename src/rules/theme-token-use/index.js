06b157c6985cc17b705af2503c60abbbd813ef2a
Lee Chase <lee.chase@uk.ibm.com>
2020-04-24 13:17:53 +0100

    feat: introduce processing of at import

--- src/rules/theme-token-use/index.js
+++ src/rules/theme-token-use/index.js
@@ -35,6 +35,148 @@ const defaultOptions = {
   ignoreValues: ["/transparent|inherit|initial/"],
 };
 
+const checkAts = (root) => {
+  let atsPromise;
+  const atsVariables = [];
+
+  root.walkAtRules((ats) => {
+    if (ats.name === "import") {
+      let filename = ats.params.substr(1, ats.params.length - 2);
+
+      if (!filename.endsWith(".scss")) {
+        filename = filename.concat(".scss");
+      }
+
+      let _filename = filename;
+      const lastSlash = filename.lastIndexOf("/");
+
+      if (lastSlash >= 0) {
+        if (filename.charAt(lastSlash + 1) !== "_") {
+          // try with _ before last name
+          _filename = filename.replace(/\/([^/]*)$/, "/_$1");
+        }
+      }
+
+      if (fs.existsSync(_filename)) {
+        filename = _filename;
+      }
+
+      const scssFromFile = fs.readFileSync(filename, "utf8");
+
+      // eslint-disable-next-line
+      console.log(scssFromFile);
+
+      atsPromise = postcss()
+        // .use(atImport()) // not working resolver struggles with extensions, try newer version
+        .process(`${scssFromFile}`, {
+          from: `${filename}`,
+          syntax: scss,
+        })
+        .then(function (result) {
+          result.root.walkDecls((decl) => {
+            if (isVariable(decl.prop)) {
+              // eslint-disable-next-line
+              console.log("prop is var");
+
+              if (checkValue(decl.value)) {
+                atsVariables[normaliseVariableName(decl.prop)] = decl.value;
+              } else {
+                // add to variable declarations
+                // expects all variables to appear before use
+                // expects all variables to be simple (not map or list)
+
+                if (isVariable(decl.value)) {
+                  // leave as is if checkValue is true
+                  // eslint-disable-next-line
+                  console.log("val is var");
+
+                  atsVariables[normaliseVariableName(decl.prop)] =
+                    atsVariables[normaliseVariableName(decl.value)];
+                } else {
+                  atsVariables[normaliseVariableName(decl.prop)] = decl.value;
+                }
+              }
+            }
+          });
+
+          return atsVariables;
+        })
+        .catch((reason) => {
+          // eslint-disable-next-line
+          console.log("Rejected ---------------", reason);
+        });
+
+      // eslint-disable-next-line
+      console.log("-------------asdfasdfasdfasdf-------");
+    }
+  });
+
+  return atsPromise;
+};
+
+const checkPropsAndValues = (root, result, options) => {
+  root.walkDecls((decl) => {
+    // // eslint-disable-next-line
+    // console.dir(decl);
+
+    if (isVariable(decl.prop)) {
+      // add to variable declarations
+      // expects all variables to appear before use
+      // expects all variables to be simple (not map or list)
+
+      variables[normaliseVariableName(decl.prop)] = decl.value;
+    } else if (checkProp(decl.prop, options.includeProps)) {
+      // is supported prop
+      // Some color properties have
+      // variable parameter lists where color can be optional
+      // variable parameters lists where color is not at a fixed position
+
+      const values = splitValueList(decl.value);
+
+      for (const value of values) {
+        if (!checkIgnoreValue(value, options.ignoreValues)) {
+          if (!checkValue(value)) {
+            // not a carbon theme token
+            if (isVariable(value)) {
+              // a variable that could be carbon theme token
+              const variableValue = variables[value];
+
+              if (!checkValue(variableValue)) {
+                // a variable that does not refer to a carbon color token
+                utils.report({
+                  ruleName,
+                  result,
+                  message: messages.rejectedVariable(
+                    decl.prop,
+                    value,
+                    variableValue
+                  ),
+                  index: declarationValueIndex(decl),
+                  node: decl,
+                });
+              }
+            } else {
+              // not a variable or a carbon theme token
+              utils.report({
+                ruleName,
+                result,
+                message: messages.rejected(decl.prop, decl.value),
+                index: declarationValueIndex(decl),
+                node: decl,
+              });
+            }
+          }
+        }
+      }
+    }
+
+    // postcss provides valueParse which we could use
+    // valueParser(decl.value).walk(node => {
+    // });
+    // }
+  });
+};
+
 export default function rule(optionsIn) {
   const options = parseOptions(optionsIn, defaultOptions);
 
@@ -53,146 +195,17 @@ export default function rule(optionsIn) {
       return;
     }
 
-    root.walkAtRules((ats) => {
-      if (ats.name === "import") {
-        let filename = ats.params.substr(1, ats.params.length - 2);
+    const atCheck = checkAts(root);
 
-        if (!filename.endsWith(".scss")) {
-          filename = filename.concat(".scss");
-        }
-
-        let _filename = filename;
-        const lastSlash = filename.lastIndexOf("/");
-
-        if (lastSlash >= 0) {
-          if (filename.charAt(lastSlash + 1) !== "_") {
-            // try with _ before last name
-            _filename = filename.replace(/\/([^/]*)$/, "/_$1");
-          }
-        }
-
-        if (fs.existsSync(_filename)) {
-          filename = _filename;
-        }
-
-        const scssFromFile = fs.readFileSync(filename, "utf8");
-
+    if (atCheck === undefined) {
+      checkPropsAndValues(root);
+    } else {
+      atCheck.then((atsVariables) => {
         // eslint-disable-next-line
-        console.log(scssFromFile, typeof scssFromFile);
+        console.dir(atsVariables);
 
-        Promise.all(
-          postcss()
-            // .use(atImport()) // not working resolver struggles with extensions, try newer version
-            .process(`${scssFromFile}`, {
-              from: `${filename}`,
-              syntax: scss,
-            })
-            .then(function (result) {
-              result.root.walkDecls((decl) => {
-                // eslint-disable-next-line
-                console.dir(decl);
-
-                if (isVariable(decl.prop)) {
-                  // eslint-disable-next-line
-                  console.log("prop is var");
-
-                  // add to variable declarations
-                  // expects all variables to appear before use
-                  // expects all variables to be simple (not map or list)
-
-                  if (isVariable(decl.value)) {
-                    // eslint-disable-next-line
-                    console.log("val is var");
-
-                    variables[normaliseVariableName(decl.prop)] =
-                      variables[normaliseVariableName(decl.value)];
-                  } else {
-                    variables[normaliseVariableName(decl.prop)] = decl.value;
-                  }
-                  // eslint-disable-next-line
-                  console.dir(variables);
-                }
-
-                // eslint-disable-next-line
-                console.log("decl");
-              });
-            })
-            .catch((reason) => {
-              // eslint-disable-next-line
-              console.log("Rejected ---------------", reason);
-            })
-        );
-
-        // eslint-disable-next-line
-        console.log("-------------asdfasdfasdfasdf-------");
-      }
-    });
-
-    // eslint-disable-next-line
-    console.log("next up");
-
-    // eslint-disable-next-line
-    console.dir(variables);
-
-    root.walkDecls((decl) => {
-      // // eslint-disable-next-line
-      // console.dir(decl);
-
-      if (isVariable(decl.prop)) {
-        // add to variable declarations
-        // expects all variables to appear before use
-        // expects all variables to be simple (not map or list)
-
-        variables[normaliseVariableName(decl.prop)] = decl.value;
-      } else if (checkProp(decl.prop, options.includeProps)) {
-        // is supported prop
-        // Some color properties have
-        // variable parameter lists where color can be optional
-        // variable parameters lists where color is not at a fixed position
-
-        const values = splitValueList(decl.value);
-
-        for (const value of values) {
-          if (!checkIgnoreValue(value, options.ignoreValues)) {
-            if (!checkValue(value)) {
-              // not a carbon theme token
-              if (isVariable(value)) {
-                // a variable that could be carbon theme token
-                const variableValue = variables[value];
-
-                if (!checkValue(variableValue)) {
-                  // a variable that does not refer to a carbon color token
-                  utils.report({
-                    ruleName,
-                    result,
-                    message: messages.rejectedVariable(
-                      decl.prop,
-                      value,
-                      variableValue
-                    ),
-                    index: declarationValueIndex(decl),
-                    node: decl,
-                  });
-                }
-              } else {
-                // not a variable or a carbon theme token
-                utils.report({
-                  ruleName,
-                  result,
-                  message: messages.rejected(decl.prop, decl.value),
-                  index: declarationValueIndex(decl),
-                  node: decl,
-                });
-              }
-            }
-          }
-        }
-      }
-
-      // postcss provides valueParse which we could use
-      // valueParser(decl.value).walk(node => {
-      // });
-      // }
-    });
+        checkPropsAndValues(root, result, options);
+      });
+    }
   };
 }
